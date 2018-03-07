93135451db16d11614053924450b470edcdf3d22
aaronlevin <vilevin@gmail.com>
2018-03-06 23:08:10 -0800

    Use the rust overlays supplied by mozilla

--- default.nix
+++ default.nix
@@ -1,23 +1,30 @@
-with import <nixpkgs> {};
-
+# with import <nixpkgs> {};
 let
-  jq = pkgs.jq;
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  jq = nixpkgs.jq;
   rdkafka = (import ./rdkafka.nix {
-    inherit stdenv fetchFromGitHub zlib perl pkgconfig python;
+    stdenv = nixpkgs.stdenv;
+    fetchFromGitHub = nixpkgs.fetchFromGitHub;
+    zlib = nixpkgs.zlib;
+    perl = nixpkgs.perl;
+    pkgconfig = nixpkgs.pkgconfig;
+    python = nixpkgs.python;
   });
-  kafka = pkgs.apacheKafka;
-  zookeeper = pkgs.zookeeper;
-  gdb = pkgs.gdb;
-  java = pkgs.jre8;
-  zlib = pkgs.zlib;
-  cargo = pkgs.cargo;
-in stdenv.mkDerivation {
+  gcc = nixpkgs.gcc;
+  kafka = nixpkgs.apacheKafka;
+  zookeeper = nixpkgs.zookeeper;
+  gdb = nixpkgs.gdb;
+  java = nixpkgs.jre8;
+  zlib = nixpkgs.zlib;
+  rust = nixpkgs.latest.rustChannels.nightly.rust;
+in nixpkgs.stdenv.mkDerivation {
 
     name = "kafka-jq";
     src = ./.;
 
     # dependencies
-    buildInputs = [ cargo jq gcc rdkafka kafka zookeeper gdb java zlib ];
+    buildInputs = [ rust jq gcc rdkafka kafka zookeeper gdb java zlib ];
 
     # how to build
     buildPhase = ''
