da2923833d592fd202fb2925ce50910d0d16e498
aaronlevin <vilevin@gmail.com>
2018-04-25 23:33:26 -0700

    Fix project to work on NixOS

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-# with import <nixpkgs> {};
+with import <nixpkgs> {};
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
@@ -13,30 +13,20 @@ let
     python = python_env;
   });
   gcc = nixpkgs.gcc;
-  libclang = nixpkgs.llvmPackages.libclang;
   kafka = nixpkgs.apacheKafka;
   zookeeper = nixpkgs.zookeeper;
   gdb = nixpkgs.gdb;
   java = nixpkgs.jre8;
   zlib = nixpkgs.zlib;
   rust = nixpkgs.latest.rustChannels.nightly.rust;
+  binutils = nixpkgs.binutils;
   pkgconfig = nixpkgs.pkgconfig;
-in nixpkgs.stdenv.mkDerivation {
-
-    name = "kafka-jq";
-    src = ./.;
-
-    # dependencies
-    buildInputs = [ rust jq libclang gcc rdkafka kafka zookeeper gdb java zlib python_env pkgconfig ];
-
-    # how to build
-    buildPhase = ''
-      make cool
-    '';
-
-    # installing after build
-    installPhase = ''
-      mkdir -p $out/bin
-      cp cool $out/bin/cool
-    '';
-}
+  libclang = nixpkgs.llvmPackages.libclang;
+in
+  llvmPackages.stdenv.mkDerivation rec {
+    name = "kafka-jq-env";
+    buildInputs = [
+      libclang rust jq gcc rdkafka kafka zookeeper gdb java zlib python_env binutils pkgconfig
+    ];
+    shellHook = "export LIBCLANG_PATH=${libclang}/lib";
+  }
